53ff20ff9ea77d066d1db0c00947d25fedd3305b
MarkKochRuge <markkochruge@gmail.com>
2024-04-04 10:37:53 +0200

    Update

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -3,3 +3,25 @@ Console.WriteLine("Hello, mig!");
 
 
 Console.WriteLine("Hello", 25);
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int arrayLength = 10;
+        int[] randomArray = new int[arrayLength];
+
+        Console.WriteLine("Random Array:");
+
+        for (int i = 0; i < arrayLength; i++)
+        {
+            randomArray[i] = random.Next(1, 101); // Generate random integers between 1 and 100
+            Console.WriteLine(randomArray[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
